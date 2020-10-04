ce547a53a84e412af7d223f22db761b0502b9ed3
ScienceTerakoya <72332636+ScienceTerakoya@users.noreply.github.com>
2020-10-04 16:44:17 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,347 @@
+function Fwd01 () {
+    _7911FB = 90
+    _81012FB = 70
+    _135Hight = 60
+    _246Hight = 100
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _7911FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _81012FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _7911FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _81012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _7911FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _81012FB + 10)
+    basic.pause(500)
+}
+function TurnL02 () {
+    _79FB = 85
+    _11FB = 75
+    _1012FB = 85
+    _8FB = 75
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function Fwd04 () {
+    _7911FB = 90
+    _81012FB = 70
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _7911FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _81012FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _7911FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _81012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _7911FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _81012FB + 10)
+    basic.pause(500)
+}
+function Fwd03 () {
+    _7911FB = 70
+    _81012FB = 90
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _7911FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _81012FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _7911FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _81012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _7911FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _81012FB + 10)
+    basic.pause(500)
+}
+function TurnL () {
+    _246UP()
+    TurnL01()
+    TurnL02()
+    TurnL03()
+}
+function Home () {
+    RightHigh = 80
+    LeftHigh = 80
+    RightFwBk = 80
+    LeftFwBk = 80
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, RightHigh)
+    basic.pause(1000)
+}
+function TurnR01 () {
+    _79FB = 75
+    _11FB = 85
+    _1012FB = 75
+    _8FB = 85
+    _135Hight = 60
+    _246Hight = 100
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function _246UP () {
+    _7911FB = 80
+    _81012FB = 80
+    _135Hight = 60
+    _246Hight = 100
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _7911FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _81012FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _7911FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _81012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _7911FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _81012FB + 10)
+    basic.pause(200)
+}
+function GoFwd () {
+    Fwd01()
+    Fwd02()
+    Fwd03()
+    Fwd04()
+}
+function TurnL03 () {
+    _79FB = 80
+    _11FB = 80
+    _1012FB = 80
+    _8FB = 80
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function Pose1 () {
+    RightHigh = 120
+    LeftHigh = 120
+    RightFwBk = 120
+    LeftFwBk = 120
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, RightHigh)
+    basic.pause(1000)
+}
+function TurnR02 () {
+    _79FB = 75
+    _11FB = 85
+    _1012FB = 75
+    _8FB = 85
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function Fwd02 () {
+    _7911FB = 70
+    _81012FB = 90
+    _135Hight = 60
+    _246Hight = 100
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _7911FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _81012FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _7911FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _81012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _7911FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _81012FB + 10)
+    basic.pause(500)
+}
+function TurnR03 () {
+    _79FB = 80
+    _11FB = 80
+    _1012FB = 80
+    _8FB = 80
+    _135Hight = 100
+    _246Hight = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function TurnR () {
+    _246UP()
+    TurnR01()
+    TurnR02()
+    TurnR03()
+}
+function TurnL01 () {
+    _79FB = 85
+    _11FB = 75
+    _1012FB = 85
+    _8FB = 75
+    _135Hight = 60
+    _246Hight = 100
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, _135Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, _246Hight)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, _79FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, _8FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, _79FB + 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, _1012FB - 10)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, _11FB)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, _1012FB + 10)
+    basic.pause(200)
+}
+function pose2 () {
+    RightHigh = 60
+    LeftHigh = 60
+    RightFwBk = 60
+    LeftFwBk = 60
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo9, LeftHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo11, RightHigh)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo12, RightHigh)
+    basic.pause(1000)
+}
+let LeftFwBk = 0
+let RightFwBk = 0
+let LeftHigh = 0
+let RightHigh = 0
+let _8FB = 0
+let _1012FB = 0
+let _11FB = 0
+let _79FB = 0
+let _246Hight = 0
+let _135Hight = 0
+let _81012FB = 0
+let _7911FB = 0
+Home()
+basic.forever(function () {
+    if (input.compassHeading() >= 215 && input.compassHeading() <= 260) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        for (let index = 0; index < 2; index++) {
+            GoFwd()
+        }
+    }
+    if (input.compassHeading() > 260) {
+        basic.showLeds(`
+            # . # . .
+            # . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        TurnL()
+    } else {
+        if (input.compassHeading() < 215) {
+            basic.showLeds(`
+                . . # . #
+                . # . . #
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+            TurnR()
+        }
+    }
+})
+basic.forever(function () {
+	
+})
